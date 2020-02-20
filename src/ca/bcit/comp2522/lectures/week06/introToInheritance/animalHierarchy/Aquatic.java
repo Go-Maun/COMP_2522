e7c526804058d2b871bb84532ce9e9905a9dfdde
Go-Maun <gomaun55@gmail.com>
2020-02-20 08:26:07 -0800

    added lecture notes code

---
+++ src/ca/bcit/comp2522/lectures/week06/introToInheritance/animalHierarchy/Aquatic.java
@@ -0,0 +1,15 @@
+package ca.bcit.comp2522.lectures.week06.introToInheritance.animalHierarchy;
+
+/**
+ * Aquatic.
+ *
+ * @author BCIT
+ * @version 2020
+ */
+public interface Aquatic {
+
+    /**
+     * Swims.
+     */
+    void swim();
+}
---
+++ src/ca/bcit/comp2522/lectures/week06/introToInheritance/roads/Direction.java
@@ -0,0 +1,11 @@
+package ca.bcit.comp2522.lectures.week06.introToInheritance.roads;
+
+/**
+ * Direction.
+ * 
+ * @author BCIT
+ * @version 2020
+ */
+public enum Direction {
+    NORTH, EAST, SOUTH, WEST;
+}
